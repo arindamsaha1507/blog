1fd59679c47c82978fdecfc290e8ba5129c7f6c8
Arindam Saha <arindamsaha1507@gmail.com>
2025-07-24 12:48:21 +0100

    Quartz sync: Jul 24, 2025, 12:48 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,23 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: "giscus",
+      options: {
+        // from data-repo
+        repo: "arindamsaha1507/blog",
+        // from data-repo-id
+        repoId: "R_kgDOPM_0fA",
+        // from data-category
+        category: "Announcements",
+        // from data-category-id
+        categoryId: "DIC_kwDOPM_0fM4CtXO7",
+        // from data-lang
+        lang: "en",
+      },
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
